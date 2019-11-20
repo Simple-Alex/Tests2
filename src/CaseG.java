863c36b2f07e1abc4f8d5e067f5ebaf91f81cc3a
Alex <sapozhnikovoleksiy@gmail.com>
2019-11-21 00:34:14 +0200

    example code

--- src/CaseG.java
+++ src/CaseG.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class CaseG {
     public static void main(String[] args) {
         int a = 5;
-        for (int i = 1;i<=10;i++){
-            int b = a*i;
-            System.out.println("5 * "+i+" = "+b);
+        for (int i = 1; i <= 10; i++) {
+            int b = a * i;
+            System.out.println("5 * " + i + " = " + b);
         }
     }
 }
--- src/CaseH.java
+++ src/CaseH.java
@@ -7,23 +7,24 @@ public class CaseH {
         int a = sc.nextInt();
         System.out.println("Количество рядов: ");
         int b = sc.nextInt();
-        for (int i= 0;a>i;i++){
+        for (int i = 0; a > i; i++) {
             System.out.print("*");
         }
-        for (int i=0;(b-2)>i;i++){
-            for (int q= 0;a>=q;q++){
-                if (q==1){
+        for (int i = 0; (b - 2) > i; i++) {
+            for (int q = 0; a >= q; q++) {
+                if (q == 1) {
                     System.out.print("*");
-                }else if (q>1&&q<a){
+                } else if (q > 1 && q < a) {
                     System.out.print(" ");
-                }else if(q==a){
+                } else if (q == a) {
                     System.out.print("*");
-                }else if (q<a){
+                } else if (q < a) {
                     System.out.println();
                 }
             }
-        }System.out.println();
-        for (int i= 0;a>i;i++){
+        }
+        System.out.println();
+        for (int i = 0; a > i; i++) {
             System.out.print("*");
         }
     }
