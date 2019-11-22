59fa9d9cdd6768e675c1db0acf55698e23a70dcd
Alex <sapozhnikovoleksiy@gmail.com>
2019-11-22 04:27:00 +0200

    example code

---
+++ src/CaseI.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CaseI {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int height = sc.nextInt();
+        for (int i = height;i>=0;i--){
+            for(int j=i;j<height; j++){
+                System.out.print("*");
+            }
+                System.out.println();
+        }for (int i = 0;(height-1)>i;i++){
+            for (int j=i;j<(height-1);j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
